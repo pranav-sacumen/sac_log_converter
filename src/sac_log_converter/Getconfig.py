d6c96eb94fdac7402ab84f95655ff9f729c132f5
amalmdas-sacumen <amalm.das@sacumen.com>
2022-06-14 09:38:22 +0530

    added get config file

--- src/sac_log_converter/Getconfig.py
+++ src/sac_log_converter/Getconfig.py
@@ -1,88 +1,73 @@
-from unittest import result
 import yaml
 from yaml.loader import SafeLoader
+import sys
+from pyparsing import Word
+from time import strftime
 
+
+def check_config(config_path):
 # Open the file and load the file
-with open('/home/dell/sac_log_converter/config/config.yaml') as f:
-    data = yaml.load(f, Loader=SafeLoader)
-    # print(data)
-    subs='present'
-    # res = [i for i in data if subs in i]
-    r = data[0]
-    print (data[0]['syslog headers']['present'])
-    if data[0]['syslog headers']['present'] == 1:
-        print("value exist for present")
-    else:
-        raise ValueError("value not found")
-    
-    if data[0]['syslog headers']['fields'] == 'timestamp,hostname':
-        print("value exist for fields")
-    else:
-        raise ValueError("value not found")  
-    print (data[1]['time format'])
-    if data[1]['time format'] == 'yyyy-MM-ddTHH:mm:ss.SSSZ.':
-        print("value exist for time format")
-    else:
-        raise ValueError("value not found")
-          
-    if data[2]['LEEF headers']['fields'] == 'Version,Vendor,Product,Version,EventID':
-        print("value exist for leef header fields")
-    else:
-        raise ValueError("value not found")
-    
-    if data[3]['delimiter'] == '|':
-        print("value exist for delimiters")
-    else:
-        raise ValueError("value not found")
-          
+    with open(config_path) as f:
+        data = yaml.load(f, Loader=SafeLoader)
+        print (data[0]['syslog headers']['present'])
+        if not data[1]['time format']:
+            raise ValueError("value not found")
+
+        if not data[2]['LEEF headers']['fields']:
+            raise ValueError("value not found")
+        
+        if not data[3]['delimiter']:
+            raise ValueError("value not found")
+        return data
 
 
-class LEEF_Logger:
-    """LEEF LOGGER"""
-    
+def parse_configurations(config -> dict):
+    class Parser(object):
+        def __init__(self):
+            ints = Word(nums)
 
-    # LEEF Headers
-    version_major = None
-    version_minor = None
-    product_vendor = None
-    product_name = None
-    product_version = None
+            # priority
+            priority = Suppress("<") + ints + Suppress(">")
 
-    def __init__(self, product_vendor, product_name, product_version,
-                 version_major=1, version_minor=0, delimiter="\t"):
-        """ Define the LEEF Headers """
+            # timestamp
+            month = Word(string.ascii_uppercase , string.ascii_lowercase, exact=3)
+            day   = ints
+            hour  = Combine(ints + ":" + ints + ":" + ints)
+            
+            timestamp = month + day + hour
 
-        self.version_major = version_major
-        self.version_minor = version_minor
-        self.product_vendor = product_vendor
-        self.product_name = product_name
-        self.product_version = product_version
+            # hostname
+            hostname = Word(alphas + nums + "_" + "-" + ".")
 
-        if delimiter not in ['\t', '|', '^']:
-            raise ValueError("Delimeter must be '\\t', '|' or '^'")
-        self.delimiter = delimiter
+            
+    def parse(self, line):
+        parsed = self.__pattern.parseString(line)
 
-    def logEvent(self, event_id, keys):
-        """
-        Log an event
-        """
-        return self._createEventString(event_id, keys)
+        payload              = {}
+        payload["priority"]  = parsed[0]
+        payload["timestamp"] = strftime("%Y-%m-%d %H:%M:%S")
+        payload["hostname"]  = parsed[4]
+        
+        
+        return payload
 
-    def _createEventString(self, event_id, keys):
-        header = self._createHeader(event_id)
-        values = sorted([(str(k) + "=" + str(v))
-                         for k, v in iter(keys.items())])
+    """ --------------------------------- """
 
-        payload = '\t'.join(values)
-
-    def _createHeader(self, event_id):
-        return "LEEF:{0}.{1}|{2}|{3}|{4}|{5}|". \
-               format(self.version_major,
-                      self.version_minor,
-                      self.product_vendor,
-                      self.product_name,
-                      self.product_version,
-                      event_id
-                      )
+def main():
+  parser = Parser()
+  
+  if len(sys.argv) == 1:
+    print("Usage:\n  $ python xlog.py ./sample.log")
+    exit(666)
+  
+  syslogPath = sys.argv[1]
+  
+  with open(syslogPath) as syslogFile:
+    for line in syslogFile:
+      fields = parser.parse(line)
+      print(fields)
+  
+if __name__ == "__main__":
+  main()
 
     
