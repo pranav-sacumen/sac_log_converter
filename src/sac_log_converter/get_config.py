4aab8c13c59829a192f982472d2b429397cf3cb3
unknown <divya.pandia@sacumen.com>
2022-06-27 15:21:16 +0530

    Added the syslog header, leef header functionality

--- src/sac_log_converter/get_config.py
+++ src/sac_log_converter/get_config.py
@@ -1,9 +1,9 @@
 """Get and Parse the configurations."""
-from datetime import datetime
 from typing import Any
 from typing import Dict
 
 import yaml
+
 
 def get_config(config_path: str) -> Dict[str, Any]:
     """Read the configurations from the yaml file.
@@ -19,33 +19,3 @@ def get_config(config_path: str) -> Dict[str, Any]:
 
 # c = get_config('C:/Users/DIVYA/Documents/Log Convertor Resuable Library/sac_log_converter-leef-json/config/config.yaml')
 # print('c:',c)
-
-def parse_headers(header:str, header_fields:str) -> Dict[str, Any]:
-    """Map headers with the header fields.
-
-    :params header: Syslog Header
-    :type header: str
-    :params header_fields: header_fields for mapping
-    :type header_fields: str
-    :return: Dictionary where syslog headers will be present
-    :rtype: str
-    """
-    header_dict = {}
-    # mapping of timestamp, hostname and version
-    # try except block to not print the error and continue
-    status = False
-    try:
-        # if timestamp is present in event, prepare a syslog header dict
-        if datetime.strptime(header.split(':')[0], '%Y') or datetime.strptime(header.split(':')[0], '%b %d %y') or datetime.strptime(header.split(':')[0], '%b %d %Y')  or datetime.strptime(header.split(':')[0], '%B %d %y') or datetime.strptime(header.split(':')[0], '%B %d %Y'):
-            header_dict[header_fields.split(',')[0]] = header.split(':')[0]
-            header_dict[header_fields.split(',')[1]] = header.split(':')[1]
-            header_dict[header_fields.split(',')[2]] = header.split(':')[2]
-            status = True
-    except ValueError:
-        pass
-        # if timestamp is not present in event, prepare a syslog header dict
-        if not status:
-            header_dict[header_fields.split(',')[1]] = header.split(':')[0]
-            header_dict[header_fields.split(',')[2]] = header.split(':')[1]
-            # logger.debug("Syslog Header Dictionary",header_dict)
-    return header_dict
--- src/sac_log_converter/utils.py
+++ src/sac_log_converter/utils.py
@@ -1,3 +1,11 @@
 from datetime import datetime
+
+
 def convert_time_to_format(str_time, format):
+    """Convert the timestamp into specified format."""
     return datetime.strftime(str_time, format)
+
+
+def check_date_time_present(time) -> datetime:
+    """Check if the timestamp is present."""
+    return datetime.strptime(time, '%Y') or datetime.strptime(time, '%b %d %y') or datetime.strptime(time, '%b %d %Y') or datetime.strptime(time, '%B %d %y') or datetime.strptime(time, '%B %d %Y')
