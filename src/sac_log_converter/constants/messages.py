6474024ef844b7eede13179ecdcfadbe2e3b36b4
pranav-sacumen <pranav.kalariya@sacumen.com>
2022-07-04 19:32:10 +0530

    Updated code to work with and without syslog headers

--- src/sac_log_converter/constants/messages.py
+++ src/sac_log_converter/constants/messages.py
@@ -1,1 +1,2 @@
 """Messages."""
+PAYLOAD_LEN_ERROR = "Payload contains more Leef Header fields than mentioned in config."
--- src/sac_log_converter/converter.py
+++ src/sac_log_converter/converter.py
@@ -7,13 +7,12 @@ from sac_log_converter.constants.config import (DELIMITER, LEEF_HEADER_FIELDS,
                                                 PAYLOAD_DELIMITER,
                                                 SYSLOG_HEADER_FIELDS,
                                                 SYSLOG_HEADER_PRESENT)
-from sac_log_converter.constants.general import (COLON, COMMA, DOUBLE_SLASH,
-                                                 EQUAL, PAY_LOAD, SLASH_EQUAL,
+from sac_log_converter.constants.general import (COMMA, DOUBLE_SLASH, EQUAL,
+                                                 PAY_LOAD, SLASH_EQUAL,
                                                  SPACE_CHAR)
-from sac_log_converter.utils import check_date_time_present
 
 
-def convert_leef_to_json(raw_config: dict, events_list: list) -> list:
+def convert_leef_to_json(raw_config: dict, events_list: dict) -> list:
     """Convert the leef format into json."""
     json_events = []
 
@@ -22,12 +21,14 @@ def convert_leef_to_json(raw_config: dict, events_list: list) -> list:
         leef_headers = {}
         # Split the event by delimiter
         event_split = event.split(raw_config[0][LEEF_TO_JSON][DELIMITER])
+        if len(event_split) != len(raw_config[0][LEEF_TO_JSON][LEEF_HEADER_FIELDS].split(",")) + 1:
+            raise ValueError("Payload contains more Leef Header fields than mentioned in config.")
         if raw_config[0][LEEF_TO_JSON][SYSLOG_HEADER_PRESENT]:
             header = event_split[0]
             # pass the header and header fields which we got from config
             syslog_headers = parse_syslog_headers(header, raw_config[0][LEEF_TO_JSON][SYSLOG_HEADER_FIELDS])
         # pass the field names from config and event split list
-        leef_headers = parse_leef_headers(raw_config[0][LEEF_TO_JSON][SYSLOG_HEADER_FIELDS], raw_config[0][LEEF_TO_JSON][LEEF_HEADER_FIELDS], event_split)
+        leef_headers = parse_leef_headers(raw_config[0][LEEF_TO_JSON][SYSLOG_HEADER_FIELDS], raw_config[0][LEEF_TO_JSON][LEEF_HEADER_FIELDS], event_split, raw_config)
         data = event_split[-1]
         # split the data using the delimeter provided in configuration
         if raw_config[0][LEEF_TO_JSON][PAYLOAD_DELIMITER]:
@@ -55,24 +56,17 @@ def parse_syslog_headers(header: str, header_fields: str) -> Dict[str, Any]:
     syslog_header_dict = {}
     # mapping of timestamp, hostname and version
     # try except block to not print the error and continue
-    status = False
-    try:
-        # if timestamp is present in event, prepare a syslog header dict
-        if check_date_time_present(header.split(COLON)[0]):
-            for index in range(len(header_fields.split(COMMA))):
-                syslog_header_dict[header_fields.split(COMMA)[index]] = header.split(COLON)[index]
-            status = True
-    except ValueError:
-        pass
-        # if timestamp is not present in event, prepare a syslog header dict
-        if not status:
-            for index in range(len(header_fields.split(COMMA)) - 1):
-                syslog_header_dict[header_fields.split(COMMA)[index + 1]] = header.split(COLON)[index]
+    header = header.split(SPACE_CHAR)
+    header_fields = header_fields.split(COMMA)
+    if len(header) - 1 != len(header_fields):
+        raise ValueError("Found different syslog headers than mentioned in config.")
+    for index in range(len(header) - 1):
+        syslog_header_dict[header_fields[index]] = header[index]
 
     return syslog_header_dict
 
 
-def parse_leef_headers(syslog_h_present: str, leef_header: str, data_list: List[str]) -> Dict[str, Any]:
+def parse_leef_headers(syslog_h_present: str, leef_header: str, data_list: List[str], config: Dict) -> Dict[str, Any]:
     """Map the leef headers with fields.
 
     :param leef_header: leef_header field names
@@ -87,16 +81,17 @@ def parse_leef_headers(syslog_h_present: str, leef_header: str, data_list: List[
     leef_header_dict = {}
     leef_header_list = leef_header.split(COMMA)
     if syslog_h_present:
-        leef_header_dict[leef_header_list[0]] = data_list[0].split(" ")[0]
+        leef_header_dict[leef_header_list[0]] = data_list[0].split(config[0][LEEF_TO_JSON][PAYLOAD_DELIMITER])[-1]
     # read the leef_header_field names and create a dictionary
         for element, index in zip(leef_header_list[1:], range(1, len(data_list))):
             leef_header_dict[element] = data_list[index]
     else:
         # read the leef_header_field names and create a dictionary
-        for element, index in zip(leef_header_list, range(1, len(data_list))):
+        for element, index in zip(leef_header_list, range(0, len(data_list))):
             leef_header_dict[element] = data_list[index]
 
     return leef_header_dict
+
 
 def parse_data(datalist: List[str]) -> Dict[str, Any]:
     """Map data and add it as a dictionary."""
